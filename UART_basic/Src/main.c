9298938d80b7ca4c717d04e8cc98e43de5402510
freude9 <75164063+freude9@users.noreply.github.com>
2020-12-15 23:13:05 +0800

    dym's homework
    uart communication
    gpio

---
+++ UART_basic/Src/main.c
@@ -0,0 +1,246 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+ 
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if(htim->Instance==htim2.Instance)
+	{
+		
+	}
+}
+static uint8_t value;
+
+int main(void)
+{
+  uint8_t value;
+  HAL_Init();
+
+ 
+  SystemClock_Config();
+
+  MX_GPIO_Init();
+  MX_UART8_Init();
+  MX_TIM2_Init();
+ HAL_UART_Receive_IT(&huart8,&value,1);
+  while (1)
+  {
+    
+  } 
+
+}
+	void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	if(huart->Instance == huart8.Instance){
+      HAL_UART_Transmit(&huart8,&value,1,0xFFFF);
+		  switch(value)
+		 {
+			case '1':HAL_GPIO_WritePin(GPIOG,GPIO_PIN_1,0);
+				       HAL_GPIO_WritePin(GPIOG,GPIO_PIN_2,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_3,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_4,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_5,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_7,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_8,1);
+			                                        break;
+			case '2':HAL_GPIO_WritePin(GPIOG,GPIO_PIN_1,1);
+				       HAL_GPIO_WritePin(GPIOG,GPIO_PIN_2,0);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_3,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_4,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_5,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_7,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_8,1);
+			                                         break;
+			case '3':HAL_GPIO_WritePin(GPIOG,GPIO_PIN_1,1);
+				       HAL_GPIO_WritePin(GPIOG,GPIO_PIN_2,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_3,0);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_4,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_5,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_7,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_8,1);
+			                                         break;
+			case '4':HAL_GPIO_WritePin(GPIOG,GPIO_PIN_1,1);
+				       HAL_GPIO_WritePin(GPIOG,GPIO_PIN_2,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_3,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_4,0);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_5,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_7,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_8,1);
+			                                         break;
+			case '5':HAL_GPIO_WritePin(GPIOG,GPIO_PIN_1,1);
+				       HAL_GPIO_WritePin(GPIOG,GPIO_PIN_2,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_3,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_4,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_5,0);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_7,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_8,1);
+			                                         break;
+			case '6':HAL_GPIO_WritePin(GPIOG,GPIO_PIN_1,1);
+				       HAL_GPIO_WritePin(GPIOG,GPIO_PIN_2,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_3,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_4,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_5,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,0);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_7,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_8,1);
+			                                         break;
+			case '7':HAL_GPIO_WritePin(GPIOG,GPIO_PIN_1,1);
+				       HAL_GPIO_WritePin(GPIOG,GPIO_PIN_2,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_3,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_4,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_5,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_7,0);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_8,1);
+			                                         break;
+			case '8':HAL_GPIO_WritePin(GPIOG,GPIO_PIN_1,1);
+				       HAL_GPIO_WritePin(GPIOG,GPIO_PIN_2,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_3,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_4,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_5,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_7,1);
+			         HAL_GPIO_WritePin(GPIOG,GPIO_PIN_8,0);
+			                                         break;
+			default:;break;
+	   }
+		 // HAL_GPIO_TogglePin(GPIOG,GPIO_PIN_3);
+	      HAL_UART_Receive_IT(&huart8,&value,1);
+	
+     }
+  }
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 6;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
